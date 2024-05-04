0d46a974bc0ff1e4f9d78e3fc516f370ed230d95
Murilo Assis <muriloaprofissional@outlook.com>
2024-05-04 05:24:30 -0300

    improved bmi and invert words

--- Inverta Palavras.py
+++ Inverta Palavras.py
@@ -2,10 +2,26 @@ def inverta_palavras():
     print('Inverta textos/palavras com esse algoritmo')
     string_para_teste = input("Digite uma palavra: ")
     string_array = []
+    palavra = False
+    list_parcial2 = []
     for letra in string_para_teste:
+
+        if letra == ' ':
+            palavra = True
+
+
         string_array.insert(0, letra)
+
     string_final = ''.join(string_array)
-    print(string_final)
+    if palavra == True:
+        list_parcial = string_final.split(" ")
+        for pal in list_parcial:
+            list_parcial2.insert(0, pal)
+        string_final2 = ' '.join(list_parcial2)
+        print(string_final2)
+    else:
+        print(string_final)
+        pass
 
 sair = False
 
--- bmi calc.py
+++ bmi calc.py
@@ -1,8 +1,3 @@
-#Variables and array needed
-
-height = float(input('Tell me your height in CENTIMETERS: '))/100
-weight  = float(input('Tell me your weight in KILOGRAMS: '))
-bmi_limits = [18.5, 25, 30]
 
 # Calculates weight
 def bmi_calc(height, weight):
@@ -19,38 +14,61 @@ def weight_needed_gain():
             return float(weight2)
 
 # Tells how much is the ideal weight for those who have to lose weight
-def weight_needed_lose():
+def weight_needed_lose_for_normal():
     weight2 = weight
     while bmi_calc(height, weight) > bmi_limits[1]:
         weight2 -= 0.1
         bmi_calc(height, weight2)
         if bmi_calc(height, weight2) < bmi_limits[1]:
             return float(weight2)
+def weight_needed_lose_for_over():
+    weight2 = weight
+    while bmi_calc(height, weight) > bmi_limits[2]:
+        weight2 -= 0.1
+        bmi_calc(height, weight2)
+        if bmi_calc(height, weight2) <= bmi_limits[2]:
+            return float(weight2)
 
-print('Your bmi is ' + str(bmi_calc(height, weight)))
-if bmi_calc(height, weight) < bmi_limits[0]:
-    
-    print('You are underweight')
-    
-    weight_to_gain = weight_needed_gain()- weight
-    #Tell how much weight the user needs to gain
-    print('You have to gain ' + str(weight_to_gain) + ' kilograms')
-    pass
-elif bmi_calc(height, weight) >= bmi_limits[0] and bmi_calc(height, weight) < bmi_limits[1]:
-    print('You have a normal weight')
-    pass
+sair = False
 
-elif bmi_calc(height, weight) >= bmi_limits[1] and bmi_calc(height, weight) < bmi_limits[2]:
-    print('You are overweight')
-    weight_to_lose = weight - float(weight_needed_lose())
-    #Tell how much weight the user needs to lose
-    print('You need to lose ' + str(weight_to_lose) + ' kilograms')
-    pass
-elif bmi_calc(height, weight) >= bmi_limits[2]:
-    weight_to_lose = weight - weight_needed_lose()
-    #Tell how much weight the user needs to lose
-    print('You need to lose ' + str(weight_to_lose) + ' kilograms')
-    print('You are obese')
-    pass
-else:
-    print('Input error')
+while sair == False:
+    #Variables and arrays needed
+    height = float(input('Tell me your height in CENTIMETERS: '))/100
+    weight  = float(input('Tell me your weight in KILOGRAMS: '))
+    bmi_limits = [18.5, 25, 30]
+    print('Your bmi is ' + str(bmi_calc(height, weight)))
+    #Conditional operations
+    if bmi_calc(height, weight) < bmi_limits[0]:
+        
+        print('You are underweight')
+        
+        weight_to_gain = weight_needed_gain()- weight
+        #Tell how much weight the user needs to gain
+        print('You have to gain ' + str(weight_to_gain) + ' kilograms')
+        pass
+    elif bmi_calc(height, weight) >= bmi_limits[0] and bmi_calc(height, weight) < bmi_limits[1]:
+        print('You have a normal weight')
+        pass
+
+    elif bmi_calc(height, weight) >= bmi_limits[1] and bmi_calc(height, weight) < bmi_limits[2]:
+        print('You are overweight')
+        weight_to_lose = weight - float(weight_needed_lose_for_normal())
+        #Tell how much weight the user needs to lose
+        print('You need to lose ' + str(weight_to_lose) + ' kilograms')
+        pass
+    elif bmi_calc(height, weight) >= bmi_limits[2]:
+        weight_to_lose = weight - weight_needed_lose_for_normal()
+        #Tell how much weight the user needs to lose
+        print('You need to lose ' + str(weight_to_lose) + ' kilograms to have a normal weight')
+        weight_to_lose2 = weight - float(weight_needed_lose_for_over())
+        print('You need to lose ' + str(weight_to_lose2) + ' kilograms to have an overweight weight')
+        print('You are obese')
+        pass
+    else:
+        print('Input error')
+    print('Do you want to continue?')
+    sim_ou_nao = input('(Y/N)?')
+    if sim_ou_nao.lower() == 'y':
+        sair = False
+    else:
+        sair = True
